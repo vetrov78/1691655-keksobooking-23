41e8714f095f25d18b9b352aba8781d6e1903948
vetrov78 <vettrov78@gmail.com>
2021-06-10 20:11:05 +0300

    Исправления наставника 1

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 const ADS_NUMBER = 10;
+const AVATAR_NUMBERS = Array.from({length: ADS_NUMBER}, (v,k)=>`0${k+1}`.slice(-2));
 const COORDINATE_ACCURACY = 5;
 const LATITUDE_MIN = 35.65000;
 const LATITUDE_MAX = 35.70000;
 const LONGITUDE_MIN = 139.70000;
 const LONGITUDE_MAX = 139.80000;
-const MAX_PRICE = 10e+7;
-const FLAT_TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const MAX_ROOMS = 10;
-const MAX_GUESTS = 15;
+const PRICE_INTERVAL = {MIN: 1, MAX: 10e+7};
+const FLAT_TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const ROOM_QUANTITY = {MIN: 1, MAX: 10};
+const GUEST_QUANTITY = {MIN: 1, MAX: 15};
 const CHECK_TIMES = ['12:00', '13:00', '14:00'];
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const PHOTOS = [
@@ -21,8 +22,11 @@ function getRandomInt (startRange, finishRange) {
   if (startRange < 0 || finishRange < 0) {
     throw new Error('Значения границ диапазона не должны быть отрицательными');
   }
-  else if (startRange >= finishRange) {
-    throw new Error('Начальная граница диапазона не должна быть больше или равна конечной');
+  else if (startRange > finishRange) {
+    throw new Error('Начальная граница диапазона не должна быть больше конечной');
+  }
+  else if (startRange === finishRange) {
+    return startRange;
   }
   return Math.floor(Math.random() * (finishRange + 1 - startRange)) + startRange;
 }
@@ -41,30 +45,28 @@ const getRandomArrayElement = (elements) => elements[getRandomInt(0, elements.le
 
 const getRandomArrayFromArray = (elements) => {
   const resultArray = [...elements];
-  const resultLength = getRandomInt(0, elements.length);
-  if (resultLength === 0) {return [];}
-  for (let it = 0; it < elements.length - resultLength; it++) {
+  const resultLength = getRandomInt(1, elements.length);
+  for (let i = 0; i < elements.length - resultLength; i++) {
     resultArray.splice(getRandomInt(0, resultArray.length - 1), 1);
   }
   return resultArray;
 };
 
-const createNew = () => {
-  const latitude = getRandomNumber(LATITUDE_MIN, LATITUDE_MAX, COORDINATE_ACCURACY);
-  const longitude = getRandomNumber(LONGITUDE_MIN, LONGITUDE_MAX, COORDINATE_ACCURACY);
+const getRandomAvatarNumber = () => AVATAR_NUMBERS.splice(getRandomInt(0, AVATAR_NUMBERS.length - 1), 1);
+
+const createNewAd = () => {
+  const lat = getRandomNumber(LATITUDE_MIN, LATITUDE_MAX, COORDINATE_ACCURACY);
+  const lng = getRandomNumber(LONGITUDE_MIN, LONGITUDE_MAX, COORDINATE_ACCURACY);
   return {
     // author, объект — описывает автора. Содержит одно поле:
-    // avatar, строка — адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} — это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются.
+    // avatar, строка — адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} — это число от 1 до 10 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются.
     author: {
-      avatar: `img/avatars/user0${getRandomInt(0,8)}.png`,
+      avatar: `img/avatars/user${getRandomAvatarNumber()}.png`,
     },
     // location, объект — местоположение в виде географических координат. Состоит из двух полей:
     // lat, число с плавающей точкой — широта, случайное значение от 35.65000 до 35.70000.
     // lng, число с плавающей точкой — долгота, случайное значение от 139.70000 до 139.80000.
-    location: {
-      lat: latitude,
-      lng: longitude,
-    },
+    location: {lat, lng},
     // offer, объект — содержит информацию об объявлении. Состоит из полей:
     // title, строка — заголовок предложения. Придумайте самостоятельно.
     // address, строка — адрес предложения. Для простоты пусть пока составляется из географических координат по маске {{location.x}}, {{location.y}}.
@@ -79,11 +81,11 @@ const createNew = () => {
     // photos, массив строк — массив случайной длины из значений: https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg.
     offer: {
       title: 'Заголовок предложения',
-      address: `${latitude}, ${longitude}`,
-      price: getRandomInt(1, MAX_PRICE),
-      type: getRandomArrayElement(FLAT_TYPE),
-      rooms: getRandomInt(1, MAX_ROOMS),
-      guests: getRandomInt(1, MAX_GUESTS),
+      address: `${lat}, ${lng}`,
+      price: getRandomInt(PRICE_INTERVAL.MIN, PRICE_INTERVAL.MAX),
+      type: getRandomArrayElement(FLAT_TYPES),
+      rooms: getRandomInt(ROOM_QUANTITY.MIN, ROOM_QUANTITY.MAX),
+      guests: getRandomInt(GUEST_QUANTITY.MIN, GUEST_QUANTITY.MAX),
       checkin: getRandomArrayElement(CHECK_TIMES),
       checkout: getRandomArrayElement(CHECK_TIMES),
       features: getRandomArrayFromArray(FEATURES),
@@ -93,6 +95,6 @@ const createNew = () => {
   };
 };
 
-const randomAdsArray = new Array(ADS_NUMBER).fill().map(createNew);
+const randomAds = new Array(ADS_NUMBER).fill().map(createNewAd);
 
-randomAdsArray();
+randomAds.values;
